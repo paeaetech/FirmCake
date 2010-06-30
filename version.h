69ab32906ce2ac9f68a4e130a16a4d118fec11a5
Mikko Sivulainen <mikko.sivulainen@symbio.com>
2010-06-30 16:42:25 +0300

    described build_id

--- version.h
+++ version.h
@@ -9,6 +9,11 @@
 #define NAME "FirmCake"
 
 
+//build_id.h contains makefile generated BUILD_ID string define. you can also define it manually
+
+//#define BUILD_ID "none"
+#ifndef BUILD_ID
 #include "build_id.h"
+#endif
 
 #endif /* end of include guard: VERSION_H_UBUGHFE1 */
