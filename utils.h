8e67770fa45b248113536b4d0027171eb6d2b556
Mikko Sivulainen <mikko.sivulainen@symbio.com>
2010-07-04 19:09:51 +0300

    fixed DEBUG macros

--- utils.h
+++ utils.h
@@ -23,7 +23,7 @@ extern UART uart3;
 #define DEBUG_OUTB(b) { uart3.send(b); }
 #define DEBUG_OUTF(f,args...) { char _buf[256]; sprintf(_buf, f,## args); DEBUG_OUT(_buf); }
 #else
-#define DEBUG_OUT(s) { for (uint8_t __x=0;s[__x];__x++) uart3.send(s[__x]); }
+#define DEBUG_OUT(s)
 #define DEBUG_OUTB(b)
 #define DEBUG_OUTF(f,args...)
 #endif
